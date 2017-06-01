0a656454bbf71419ad4f8a3811645b0d9d0cfaab
Erter <mrmuff@icloud.com>
2017-05-31 17:01:39 -0700

    Playing with creating Security Groups, ingress and egress rules.

---
+++ multi-sg.tf
@@ -0,0 +1,163 @@
+provider "aws" {
+  access_key = "AKIAJAOV3NDVZL3M2RNA"
+  secret_key = "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"
+  region     = "us-west-1"
+}
+resource "aws_security_group" "dc-security-group" {
+  name        = "dc-security-group"
+  description = "Allow Domain Controller ports and protocols inbound"
+
+  ingress {
+    from_port   = 464
+    to_port     = 464
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 464
+    to_port     = 464
+    protocol    = "udp"
+    cidr_blocks = ["10.237.0.0/16"]
+  }
+  ingress {
+    from_port   = 49152
+    to_port     = 65535
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 49152
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 5666
+    to_port     = 5666
+    protocol    = "tcp"
+    cidr_blocks = ["10.25.0.0/16"]
+  }
+  ingress {
+    from_port   = 445
+    to_port     = 445
+    protocol    = "tcp"
+    cidr_blocks = ["10.70.0.0/16"]
+  }
+  ingress {
+    from_port   = 445
+    to_port     = 445
+    protocol    = "tcp"
+    cidr_blocks = ["10.237.0.0/16"]
+  }
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.40.4.0/24"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.40.5.0/24"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["141.206.246.10/32"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.25.0.0/16"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.26.0.0/16"]
+  }
+  ingress {
+    from_port   = 5985
+    to_port     = 5985
+    protocol    = "tcp"
+    cidr_blocks = ["10.70.4.0/22"]
+  }
+  ingress {
+    from_port   = 135
+    to_port     = 135
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 139
+    to_port     = 139
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 3269
+    to_port     = 3269
+    protocol    = "tcp"
+    cidr_blocks = ["10.44.4.0/24"]
+  }
+  ingress {
+    from_port   = 3269
+    to_port     = 3269
+    protocol    = "tcp"
+    security_groups = ["sg-8f8db4e8"]
+  }
+  ingress {
+    from_port   = 445
+    to_port     = 445
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 138
+    to_port     = 138
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = 138
+    to_port     = 138
+    protocol    = "udp"
+    security_groups = ["sg-8f8db4e8"]
+  }
+  egress {
+     from_port = 0
+     to_port = 0
+     protocol = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "dc-security-group"
+  }
+}
